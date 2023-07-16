2b2c894d53b96556b5c40616d12908e4a8f3f6c5
roaaab23-meet <109908183+roaaab23-meet@users.noreply.github.com>
2023-07-16 19:11:08 +0300

    jj
    jb

---
+++ app/src/main/java/com/example/simanimvp/iphone_14___sign_in_page_activity.java
@@ -0,0 +1,64 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		iphone_14___sign_in_page
+	 *	@date 		Sunday 16th of July 2023 01:36:14 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class iphone_14___sign_in_page_activity extends Activity {
+
+	
+	private View _bg__iphone_14___sign_in_page_ek2;
+	private ImageView ellipse_1;
+	private View rectangle_87;
+	private TextView save_ek1;
+	private ImageView logo;
+	private View rectangle_94;
+	private TextView arabic_ek1;
+	private View rectangle_94_ek1;
+	private TextView hebrew_ek1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.iphone_14___sign_in_page);
+
+		
+		_bg__iphone_14___sign_in_page_ek2 = (View) findViewById(R.id._bg__iphone_14___sign_in_page_ek2);
+		ellipse_1 = (ImageView) findViewById(R.id.ellipse_1);
+		rectangle_87 = (View) findViewById(R.id.rectangle_87);
+		save_ek1 = (TextView) findViewById(R.id.save_ek1);
+		logo = (ImageView) findViewById(R.id.logo);
+		rectangle_94 = (View) findViewById(R.id.rectangle_94);
+		arabic_ek1 = (TextView) findViewById(R.id.arabic_ek1);
+		rectangle_94_ek1 = (View) findViewById(R.id.rectangle_94_ek1);
+		hebrew_ek1 = (TextView) findViewById(R.id.hebrew_ek1);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
