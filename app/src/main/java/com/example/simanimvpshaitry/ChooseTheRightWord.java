aa7be75c418084efbbe6dd665545c833f4ad7458
shai linde <shaylinde1@gmail.com>
2023-07-27 14:45:27 +0300

    before new medical vocab

--- app/src/main/java/com/example/simanimvpshaitry/ChooseTheRightWord.java
+++ app/src/main/java/com/example/simanimvpshaitry/ChooseTheRightWord.java
@@ -22,7 +22,6 @@ public class ChooseTheRightWord extends AppCompatActivity implements View.OnClic
     private Button option3;
     private Button option4;
     private ImageButton buttonBack;
-    private Button submitAnswerButton;
     ImageView video;
     private String answer;
     @SuppressLint("MissingInflatedId")
@@ -35,14 +34,12 @@ public class ChooseTheRightWord extends AppCompatActivity implements View.OnClic
         option3=findViewById(R.id.option3);
         option4=findViewById(R.id.option4);
         buttonBack=findViewById(R.id.backbutton);
-        submitAnswerButton=findViewById(R.id.submitAnswerButton);
         video=findViewById(R.id.schoolgif);
         option1.setOnClickListener(this);
         option2.setOnClickListener(this);
         option3.setOnClickListener(this);
         option4.setOnClickListener(this);
         buttonBack.setOnClickListener(this);
-        submitAnswerButton.setOnClickListener(this);
         gifMake(video);
     }
     public void gifMake(ImageView gif){
@@ -59,41 +56,14 @@ public class ChooseTheRightWord extends AppCompatActivity implements View.OnClic
 
     @Override
     public void onClick(View view) {
-        if(view==option1){
-            ChangeColorButton(option2,option3,option4);
-            option1.setBackgroundColor(Color.MAGENTA);
-            answer="1";
-        }
-        if(view==option2){
-            ChangeColorButton(option1,option3,option4);
-            option2.setBackgroundColor(Color.MAGENTA);
-            answer="2";
-        }
-        if(view==option3){
-            ChangeColorButton(option2,option1,option4);
-            option3.setBackgroundColor(Color.MAGENTA);
-            answer="3";
-        }
         if(view==option4){
-            ChangeColorButton(option2,option3,option1);
-            option4.setBackgroundColor(Color.MAGENTA);
-            answer="4";
+            Intent i = new Intent(this, learnTheWord2.class);
+            startActivity(i);
         }
         if(view==buttonBack){
             Intent i = new Intent(this, WriteTheSentenceInHebrew.class);
             startActivity(i);
         }
-        if(view==submitAnswerButton){
-            if(answer.equals("4")){
-                Intent i = new Intent(this, learnTheWord2.class);
-                startActivity(i);
-            }
-        }
-    }
-    public void ChangeColorButton(Button bt1,Button bt2,Button bt3)
-    {
-        bt1.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_500));
-        bt2.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_500));
-        bt3.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_500));
+
     }
 }
